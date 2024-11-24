d4dd4f0ea4f39dbfa07775ef350d8f958cf302eb
Arjun Ashok <arjun3.ashok@gmail.com>
2024-11-24 11:37:10 -0800

    setup for the interface

---
+++ src/arch/interface.py
@@ -0,0 +1,28 @@
+"""
+@brief Interface for the model architectures; all models used should follow the 
+    same structure to ensure compatability.
+@author Arjun Ashok (arjun3.ashok@gmail.com)
+"""
+
+
+# --- Environment Setup --- #
+# external modules
+import torch
+import torch.nn as nn
+import torch.functional as F
+import numpy as np
+import pandas as pd
+
+# built-in modules
+## none for now..
+
+# internal modules
+## none for now...
+
+
+# --- Interface --- #
+class CVModel(nn.Module):
+    pass
+
+
+
