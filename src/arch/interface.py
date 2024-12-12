981c101fc11532f58178e55bc5c6eb492c6272db
Arjun Ashok <arjun3.ashok@gmail.com>
2024-12-12 14:19:27 -0800

    updated requirements to match newest intepreter

--- src/arch/interface.py
+++ src/arch/interface.py
@@ -13,11 +13,6 @@ import torch.functional as F
 import torch.optim as optim
 import numpy as np
 import cv2
-# from pytorch_grad_cam import GradCAM
-# from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-# from pytorch_grad_cam.utils.image import show_cam_on_image, \
-#     deprocess_image, \
-#     preprocess_image
 from gradcam import GradCAM
 from gradcam.utils import visualize_cam
 from tqdm import tqdm
